0d71ff0589accd8e725e2c1a5cd3efb33aedecb4
LeDinhNguyen <ledinhnguyen00@gmail.com>
2023-06-18 12:17:26 +0700

    update methods in binary tree

--- CSD203/BinaryTree.py
+++ CSD203/BinaryTree.py
@@ -19,20 +19,40 @@ class BinaryTree:
             else:
                 return right_height + 1
     
-    def nodeLevel(self, node: Node):
-        pass
+    def nodeLevel(self, node: Node, level: int):
+        if node is None:
+            return 0
+        elif level == 1:
+            print(node.data, end=" ")
+        else:
+            self.nodeLevel(node.left, level - 1)
+            self.nodeLevel(node.right, level - 1)
     
     def levelOrder(self, node: Node):
-        pass
+        height = self.height(node)
+        for i in range(height):
+            self.nodeLevel(node, i + 1)
+            print()
+
 
     def inOrder(self, node: Node):
-        pass
+        if node:
+            self.inOrder(node.left)
+            print(node.data, end=" ")
+            self.inOrder(node.right)
+
 
     def preOrder(self, node: Node):
-        pass
+        if node:
+            print(node.data, end=" ")
+            self.preOrder(node.left)
+            self.preOrder(node.right)
     
     def postOrder(self, node: Node):
-        pass
+        if node:
+            self.postOrder(node.right)
+            self.postOrder(node.left)
+            print(node.data, end=" ")
 
 if __name__ == "__main__":
     root = Node(1)
@@ -40,6 +60,24 @@ if __name__ == "__main__":
     root.right = Node(3)
     root.left.left = Node(4)
     root.left.right = Node(5)
-    root.left.right.left = Node(6)
+    root.right.left = Node(6)
+    root.right.right = Node(7)
+    root.left.left.left = Node(8)
+    root.left.left.right = Node(9)
+    root.left.right.left = Node(10)
+    root.left.right.right = Node(11)
+    root.right.left.left = Node(12)
+    root.right.left.right = Node(13)
+    root.right.right.left = Node(14)
+    root.right.right.right = Node(15)
     b = BinaryTree(root)
-    print(b.height(root))
+    print("Inorder Traversal:", end=" ")
+    b.inOrder(root)
+    print()
+    print("Preorder Traversal:", end=" ")
+    b.preOrder(root)
+    print()
+    print("Postorder Traversal:", end=" ")
+    b.postOrder(root)
+    print()
+    b.levelOrder(root)
