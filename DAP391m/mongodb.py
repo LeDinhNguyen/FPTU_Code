a4c2fb4cb08a44f1236ed1674d0c6fbb85df15dd
LeDinhNguyen <ledinhnguyen00@gmail.com>
2024-01-23 22:03:36 +0700

    save

--- DAP391m/mongodb.py
+++ DAP391m/mongodb.py
@@ -1,7 +1,8 @@
 import pymongo
 import os
 import pandas as pd
-import json
+from concurrent.futures import ThreadPoolExecutor
+import time
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017")
 
@@ -26,11 +27,10 @@ for i in data_file:
 data = pd.read_csv("./data/cleaned_keywords.csv", index_col=0)
 
 def mongoimport(csv_path, collection):
-    data = pd.read_csv(csv_path, index_col=0)
-    temp = []
-    for i in range(data.shape[0]):
-        temp.append(data.iloc[i].to_dict())
-    collection.insert_many(temp)
+    df = pd.read_csv(csv_path, index_col=0)
+    data = df.to_dict(orient='records')
+    collection.insert_many(data)
+
 
 for d in data_file:
     if collections_name[d] not in mydb.list_collection_names():
@@ -39,3 +39,21 @@ for d in data_file:
     print(f"******Start {collections_name[d]}******")
     mongoimport(f"./data/{d}", curr_coll)
     print(f"******Done {collections_name[d]}******\n")
+
+    chunk_size = 1000
+    data_chunks = [data[i:i + chunk_size] for i in range(0, len(data), chunk_size)]
+
+    # Number of threads (adjust as needed)
+    num_threads = 4
+
+    start_time = time.time()
+
+    # Use ThreadPoolExecutor for multithreading
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        executor.map(mongoimport, data_chunks)
+
+    end_time = time.time()
+
+    # Calculate and print the total running time
+    total_time = end_time - start_time
+    print(f"Total running time: {total_time} seconds")
